dd439987516abee237086549c45e396f8a26ffd8
Sharvan TG <sharvan@plumhq.com>
2023-04-13 11:16:27 +0530

    Committing changes

--- src/hs.js
+++ src/hs.js
@@ -1,25 +1,25 @@
-// import { values } from './index.js';
+import axios from 'axios';
 
-// import axios from 'axios';
+import { values } from './index.js';
 
-// const properties = values;
+const properties = values;
 
-// function sendDataToHS() {
-//   axios({
-//     method: 'POST',
-//     url: 'https://api.hubspot.com/crm/v3/objects/contacts',
-//     headers: {
-//       'Content-Type': 'application/json',
-//       Authorization: `Bearer <API TOKEN>`,
-//     },
-//     data: {
-//       properties,
-//     },
-//   })
-//     .then((response) => {
-//       console.log(response.data);
-//     })
-//     .catch((error) => {
-//       console.error(error);
-//     });
-// }
+function sendDataToHS() {
+  axios({
+    method: 'POST',
+    url: 'https://api.hubspot.com/crm/v3/objects/contacts',
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer <API TOKEN>`,
+    },
+    data: {
+      properties,
+    },
+  })
+    .then((response) => {
+      console.log(response.data);
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
